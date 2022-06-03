5f11522f6c2c7b11a027e8b4d827e83bfa5b811a
lawson2000 <kanyugilawson17@gmail.com>
2022-06-02 20:37:53 -0700

    hello c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print the strings that inputs the function
+ * Description:printing "programming is like building a multiligual puzzle"
+ * Return:0
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
