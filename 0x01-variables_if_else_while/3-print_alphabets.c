92f780ef955cc05d113857f93f6aac78717f94f5
lawson2000 <kanyugilawson17@gmail.com>
2022-06-06 08:55:46 -0700

    lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
+	
 	char ch;
 	for (ch ='a' ; ch <= 'z' ; ch++)
 	{
-		char ch;
-	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return(0);
+	char(ch);
+         	}		
+		for (ch = 'A' ; ch <= 'Z' ; ch++)
+        	{	
+			putchar(ch);
+        	}
+			putchar('\n');
+		return(0);
 }
