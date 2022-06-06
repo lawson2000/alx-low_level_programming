1be380616b88bf3cff28633fdadddc8baa7cb714
lawson2000 <kanyugilawson17@gmail.com>
2022-06-06 11:31:40 -0700

    Molet

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - return integal of 0
  *
- * Return: Always 0 (success)
+ * Description: print the size of different characters and words
+ *
+ * Return: Always success return 0
+ *
  */
 
 int main(void)
 {
-
 	printf("size of a char: %ld byte(s)\n", sizeof(acharacter));
 	printf("size of an int: %ld byte(s)\n", sizeof(ainteger));
 	printf("size of a long int: %ld byte(s)\n", sizeof(along));
