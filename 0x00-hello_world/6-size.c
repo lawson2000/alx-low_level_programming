068adbb49e513e585aba0bc46c2e48269f8bc972
lawson2000 <kanyugilawson17@gmail.com>
2022-06-06 02:41:28 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,11 @@
 
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
-
-	printf(stderr, "size of a char: 1 byte(s)\n", sizeof(acharacter));
-	printf(stderr, "size of an int: 4 byte(s)\n", sizeof(ainteger));
-	printf(stderr, "size of a long int: 4 byte(s)\n", sizeof(along));
-	printf(stderr, "size of a long long int: 8 byte(s)\n", sizeof(alonglong));
-	printf(stderr, "size of a float: 4 byte(s)\n", sizeof(afloat));
+	printf("size of a char: 1 byte(s)\n");
+	printf("size of an int: 4 byte(s)\n");
+	printf("size of a long int: 4 byte(s)\n");
+	printf("size of a long long int: 8 byte(s)\n");
+	printf("size of a float: 4 byte(s)\n");
 
 	return (0);
 }
