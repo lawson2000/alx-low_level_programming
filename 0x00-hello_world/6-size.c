455f5cf6e6abfb8690e4d5a0ae7d9eb283c13dac
lawson2000 <kanyugilawson17@gmail.com>
2022-06-06 02:46:20 -0700

    size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("size of a char: 1 byte(s)\n", stderr);
-	printf("size of an int: 4 byte(s)\n", stderr);
-	printf("size of a long int: 4 byte(s)\n", stderr);
-	printf("size of a long long int: 8 byte(s)\n", stderr);
-	printf("size of a float: 4 byte(s)\n");
+	fprintf("size of a char: 1 byte(s)\n", stderr);
+	fprintf("size of an int: 4 byte(s)\n", stderr);
+	fprintf("size of a long int: 4 byte(s)\n", stderr);
+	fprintf("size of a long long int: 8 byte(s)\n", stderr);
+	fprintf("size of a float: 4 byte(s)\n");
 
 	return (0);
 }
