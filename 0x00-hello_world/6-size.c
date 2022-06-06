902c65cb8f4ea9de659db131f4faed9a0c34acee
lawson2000 <kanyugilawson17@gmail.com>
2022-06-05 21:57:46 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,12 @@ int main(void)
 	long along;
 	long long alonglong;
 	float afloat;
+
 	printf(stderr, "size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf(stderr, "size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf(stderr, "size of a long int: %lu byte(s)\n", sizeof(along));
 	printf(stderr, "size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf(stderr, "size of a float: %lu byte(s)\n", sizeof(afloat));
+
 	return (0);
 }
