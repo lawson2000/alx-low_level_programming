c9600ba48e15e40eb4482c090dd2d89c61bb04c5
lawson2000 <kanyugilawson17@gmail.com>
2022-06-06 09:57:01 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ *
+ * main - print all single digit numbers using putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number + '0');
+	putchar('\n');
+	return (0);
+}
