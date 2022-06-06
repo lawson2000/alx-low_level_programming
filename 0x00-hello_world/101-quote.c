aadc9e16f5565eb62a54f542bcbce82c2fc8ba58
lawson2000 <kanyugilawson17@gmail.com>
2022-06-05 21:47:08 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main -use to return the integer value 1
  *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19' 
+ * Description:use to print to standard output in the main function
  *
- * Return: always 0(success)
+ * Return: return the value 1
  */
+
 int main(void)
+
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
