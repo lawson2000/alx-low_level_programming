ca9923703df0dde5586952eba3bf042c7e2e9d67
lawson2000 <kanyugilawson17@gmail.com>
2022-06-08 10:58:24 -0700

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
