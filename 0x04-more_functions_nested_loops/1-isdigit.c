07b1b82daa804c1ddb5cc52c5cfc08eec6c29711
lawson2000 <kanyugilawson17@gmail.com>
2022-06-09 07:13:48 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+include "main.h"
+/*
+ * _isdigit  - check if parameter is between 0 to 9
+ * @c: input number
+ * Return: 1 if is a number (0 to 9), 0 in other case
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
