2059e8d9ec1549e8caa3cab280f66b8db8e03408
lawson2000 <kanyugilawson17@gmail.com>
2022-06-13 09:24:00 -0700

    assign 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - assigns value of 98 to a pointer
+ * @n - pointer used
+ *
+ * Return: (0)
+ */
+voi reset_to_98(int *n)
+{
+	int n;
+	n = 402;
+
+	printf(" n = %d\n", n);
+		*n = 98;
+		printf(" n = %d\n", n);
+		return (0);
+}
+
