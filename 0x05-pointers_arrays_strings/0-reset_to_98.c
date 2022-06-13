bfd9c366206a008bdd8b15b3dbe46adaab7216cf
lawson2000 <kanyugilawson17@gmail.com>
2022-06-13 09:46:28 -0700

    assign 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,10 @@
-#include "main.h"
+#include "main.c"
 /**
- * reset_to_98 - assigns value of 98 to a pointer
- * @n - pointer used
- *
- * Return: (0)
- */
-voi reset_to_98(int *n)
+* reset_to_98 -a function that takes a pointer to an int
+* @n: puntero
+* Return: (0)
+*/
+void reset_to_98(int *n)
 {
-	int n;
-	n = 402;
-
-	printf(" n = %d\n", n);
-		*n = 98;
-		printf(" n = %d\n", n);
-		return (0);
+	*n = 98;
 }
-
